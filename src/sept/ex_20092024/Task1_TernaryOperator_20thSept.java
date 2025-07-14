bc6d7572cb6abf7c78f9268b37f5c6a4c2fe0fad
Kavita <banekavita26@gmail.com>
2025-07-14 15:37:34 +0530

    All files - 20th Sept Task1

--- src/sept/ex_20092024/Task1_TernaryOperator_20thSept.java
+++ src/sept/ex_20092024/Task1_TernaryOperator_20thSept.java
@@ -4,10 +4,13 @@ public class Task1_TernaryOperator_20thSept {
     public static void main(String[] args) {
         //✅ By using Ternary Operators max between 3 numbers.
         int a = 90 ;
-        int b = 25;
-        int c = 45;
+        int b = 35;
+        int c = 105;
         int result= a>b? (a>c ? a:c):(b>c ? b:c);
         System.out.println(result);
-
+//OR
+        int max = a>b ? a:b;
+        int maxno = max>c ? max : c;
+        System.out.println("Max No :" +maxno);
     }
 }
