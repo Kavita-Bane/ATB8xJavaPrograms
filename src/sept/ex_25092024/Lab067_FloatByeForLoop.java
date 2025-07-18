1e96ad7d9ab7c8720c9b7a9419b76e30fcff9f6d
Kavita <banekavita26@gmail.com>
2025-07-18 20:38:42 +0530

    All files - 25th Sept Task1

---
+++ src/sept/ex_25092024/Lab067_FloatByeForLoop.java
@@ -0,0 +1,12 @@
+package ex_25092024;
+
+public class Lab067_FloatByeForLoop {
+    public static void main(String args[]){
+        for (float f = 0.0f; f < 10.67; f++){
+            System.out.println("Hi, float: " +f);
+        }
+        for (byte b = 0; b<4; b++){
+            System.out.println("Hi, byte: " +b);
+        }
+    }
+}
